8b8681c6d0c4c1eb5b3862f7a7dd19329cfee7b9
Matías Nicolás Ortiz <88355010+MatiOrtiz@users.noreply.github.com>
2024-02-07 11:41:07 -0300

    h2 mem started

--- src/main/java/com/masterspringspringboot/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/masterspringspringboot/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth->{
@@ -20,5 +24,20 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic(withDefaults());
         http.csrf(csrf-> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var user= User.withUsername("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        var admin= User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
